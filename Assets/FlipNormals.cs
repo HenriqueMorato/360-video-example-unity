5edc612b028654e6dc86d28f1b553a756699dc74
HenriqueMorato <riquemorato@gmail.com>
2017-10-16 23:43:53 -0200

    Project Implemented, Build for Google Cardboard

---
+++ Assets/FlipNormals.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class FlipNormals : MonoBehaviour { 
+ 
+ void Awake () {
+     	Mesh mesh = GetComponent<MeshFilter>().mesh;
+ 		mesh.triangles = mesh.triangles.Reverse().ToArray();
+     }
+     
+ }
